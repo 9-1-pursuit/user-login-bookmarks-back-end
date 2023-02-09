1f148dbcaa85dc67a8c9823da07b1f8bc65d4163
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-02-08 19:00:04 -0500

    update for login

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,6 +1,9 @@
 // http://vitaly-t.github.io/pg-promise/module-pg-promise.html
-const pgp = require("pg-promise")();
-require("dotenv").config();
+const pgp = require('pg-promise')();
+// import pgp from 'pg-promise'
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+require('dotenv').config();
 
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
   process.env;
@@ -10,19 +13,17 @@ const cn = DATABASE_URL
       connectionString: DATABASE_URL,
       max: 30,
       ssl: {
-        rejectUnauthorized: false,
-      },
+        rejectUnauthorized: false
+      }
     }
   : {
       host: PG_HOST,
       port: PG_PORT,
       database: PG_DATABASE,
       user: PG_USER,
-      password: PG_PASSWORD,
+      password: PG_PASSWORD
     };
 
 const db = pgp(cn);
-
-console.log(cn);
 
 module.exports = db;
