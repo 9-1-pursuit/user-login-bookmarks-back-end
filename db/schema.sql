66fdc1833eae93088d83cb48ec897d3efd441087
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-02-10 17:40:19 -0500

    new schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,11 @@ CREATE DATABASE bookmarks_user_api;
 
 DROP TABLE IF EXISTS bookmarks CASCADE;
 
+DROP TABLE IF EXISTS users_bookmarks;
+DROP TABLE IF EXISTS reviews;
+
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE bookmarks (
     id SERIAL PRIMARY KEY, 
     name TEXT NOT NULL, 
@@ -15,18 +20,26 @@ CREATE TABLE bookmarks (
 );
 
 
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     username TEXT UNIQUE NOT NULL, 
     password TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS users_bookmarks;
-
 CREATE TABLE users_bookmarks (
     created TIMESTAMP WITH TIME ZONE,
     bookmark_id INTEGER,
     user_id INTEGER
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY, 
+    reviewer TEXT, 
+    title TEXT,
+    content TEXT,
+    rating NUMERIC,
+    CHECK (rating >= 0 AND rating <= 5),
+    bookmark_id INTEGER references bookmarks (id),
+    user_id INTEGER references users (id)
+    ON DELETE CASCADE
 );
