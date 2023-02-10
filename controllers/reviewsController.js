8b40fa4ed04781127f876eaf37a2bc0ef4aa95ee
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-02-10 17:38:52 -0500

    return reviews

---
+++ controllers/reviewsController.js
@@ -0,0 +1,79 @@
+const express = require("express");
+
+const { getBookmark } = require("../queries/bookmarks.js");
+const reviews = express.Router({ mergeParams: true });
+const {
+  getAllReviews,
+  getReview,
+  newReview,
+  deleteReview,
+  updateReview,
+} = require("../queries/reviews");
+
+// INDEX
+reviews.get("/", async (req, res) => {
+  const { bookmarkId } = req.params;
+  console.log("reviews", bookmarkId);
+  const allReviews = await getAllReviews(bookmarkId);
+  if (allReviews[0]) {
+    res.status(200).json(allReviews);
+  } else {
+    res.status(200).json({ message: "no reviews" });
+  }
+});
+
+// SHOW
+reviews.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("get reviews", id);
+  const review = await getReview(id);
+  console.log("reviews", review);
+  if (review) {
+    res.json(review);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+reviews.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("get reviews", id);
+  const review = await getReview(id);
+  console.log("reviews", review);
+  if (review) {
+    res.json(review);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// UPDATE
+reviews.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedReview = await updateReview(id, req.body);
+  if (updatedReview.id) {
+    res.status(200).json(updatedReview);
+  } else {
+    res.status(404).json("Review not found");
+  }
+});
+
+reviews.post("/", async (req, res) => {
+  console.log("hello bookmark", req.body);
+  const review = await newReview(req.body);
+  res.status(200).json(review);
+});
+
+// DELETE
+reviews.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const deletedReview = await deleteReview(id);
+  if (deletedReview.id) {
+    res.status(200).json(deletedReview);
+  } else {
+    res.status(404).json({ error: "Review not found" });
+  }
+});
+
+module.exports = reviews;
